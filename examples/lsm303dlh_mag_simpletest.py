800f0a6776f50699e19c03d810eadde5b074db19
dherrada <dylan.herrada@gmail.com>
2020-03-16 16:33:45 -0400

    Ran black, updated to pylint 2.x

--- examples/lsm303dlh_mag_simpletest.py
+++ examples/lsm303dlh_mag_simpletest.py
@@ -11,6 +11,10 @@ sensor = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
 while True:
     mag_x, mag_y, mag_z = sensor.magnetic
 
-    print('Magnetometer (gauss): ({0:10.3f}, {1:10.3f}, {2:10.3f})'.format(mag_x, mag_y, mag_z))
-    print('')
+    print(
+        "Magnetometer (gauss): ({0:10.3f}, {1:10.3f}, {2:10.3f})".format(
+            mag_x, mag_y, mag_z
+        )
+    )
+    print("")
     time.sleep(1.0)
