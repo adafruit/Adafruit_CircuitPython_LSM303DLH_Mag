259c99431bf759d2de7d73048bee91efce79eb5b
dewalex <laldew@gmail.com>
2021-05-31 14:33:26 -0400

    Update lsm303dlh_mag_tiltcompensatedcompass.py
    Reran pylint

--- examples/lsm303dlh_mag_tiltcompensatedcompass.py
+++ examples/lsm303dlh_mag_tiltcompensatedcompass.py
@@ -3,17 +3,21 @@
 # SPDX-License-Identifier: MIT
 
 # The equations below are the ones used in code
-# Equations from st manual, "Computing tilt measurement and tilt-compensated eCompass" by Andrea Vitali
+# Equations from st manual:
+# "Computing tilt measurement and tilt-compensated eCompass" by Andrea Vitali
 # https://www.st.com/resource/en/design_tip/dm00269987-computing-tilt-measurement-and-tiltcompensated-ecompass-stmicroelectronics.pdf
 
 # for Phi (Roll)
-# Phi = atan2(Gy, Gz) # for stability, 'Gz' can be substituted with Gz+Gx*alpha (with alpha=0.01-0.05)
+# Phi = atan2(Gy, Gz)
+# for stability, 'Gz' can be substituted with Gz+Gx*alpha (alpha=0.01-0.05)
 
 # for Theta (Pitch)
 # Gz2 = Gy * Sin( Phi ) + Gz * Cos( Phi )
 # Theta = Atan(-Gx / Gz2)
 
-# for Psi or "Yaw" (The Tilt Compensated Heading) # Bx,By,Bz are magnetometer readings
+# for Psi or "Yaw" (The Tilt Compensated Heading)
+# Bx,By,Bz are magnetometer readings
+
 # By2 = Bz * Sin( Phi ) – By * Cos( Phi )
 # Bz2 = By * Sin( Phi ) + Bz * Cos( Phi )
 # Bx3 = Bx * Cos( Theta ) + Bz2 * Sin( Theta )
@@ -30,7 +34,7 @@ import adafruit_lsm303_accel
 import adafruit_lis2mdl
 import adafruit_lsm303dlh_mag
 
-# Mag and Accel offsets from calibration. You will need to calibrate for your own offsets
+# Mag and Accel offsets from calibration. You need to calibrate your own offset
 offmag = (-10.8, 4.875, -54.225)
 offaccel = (-0.994392, -0.305969, 0.382458)
 
@@ -73,7 +77,7 @@ while True:
     print("Yaw Heading: ", "{:.0f}".format(Psi))
 
     # Offset declination for your general location
-    # negative 5 degrees declination in FL  ## find yours @ magnetic-declination.com
+    # -5 degrees declination in FL ## find yours @ magnetic-declination.com
     withdecl = Psi - 5
     if withdecl < 0:
         withdecl += 360
